0754b65e56d54b87e3408dc1005dea47bfbf40e0
lisandroveron <lisandro.veron@hotmail.com>
2022-05-03 17:16:08 -0300

    Added: Event Listener to the calculator keyboard, now pressing the buttons will set their values in the screen.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const calculator = document.getElementById("calculator");
+const keyboard = document.getElementById("keyboard");
+let writeLine = document.getElementById("writeLine");
+writeLine.value = "";
+
+// Listener to the keyboard, for set numbers in the screen
+keyboard.addEventListener("click", (e)=>{
+	if(e.target.value != undefined && writeLine.textLength <= 21){
+		writeLine.value += e.target.value;
+	} else{};
+});
