3533ca2f045c08a596f90d520bf075d6eecf6f20
lisandroveron <lisandro.veron@hotmail.com>
2022-05-04 20:16:49 -0300

    Added: C, Erase and Equal button functions.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
+// Declarations to track the elements of the html
+const calculator = document.getElementById("calculator");
 const keyboard = document.getElementById("keyboard");
 let writeLine = document.getElementById("writeLine");
+let resultLine = document.getElementById("resultLine");
 writeLine.value = "";
 
-// Listener to the keyboard, for set numbers in the screen
-keyboard.addEventListener("click", (e)=>{
-	if(e.target.value != undefined && writeLine.textLength <= 21){
+let result;
+// Listener to the calculator keyboard
+calculator.addEventListener("click", (e)=>{
+	if(e.target.type == "button" && e.target.value != undefined && writeLine.textLength <= 21 && e.target.value != "=" && e.target.value != "C" && e.target.value != "<"){
 		writeLine.value += e.target.value;
-	} else{};
+	} else if(e.target.value == "="){
+		result = eval(writeLine.value);
+		resultLine.value = result;
+	} else if(e.target.value == "C"){
+		resultLine.value = "";
+		writeLine.value = "";
+	} else if(e.target.value == "<"){
+		writeLine.value = writeLine.value.slice(0, -1);
+	};
 });
